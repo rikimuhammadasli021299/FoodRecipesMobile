9ed4f9b7cb26c2144c5aa1d23eedeccf8552d4b2
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-12-22 23:24:43 +0700

    add push notifications with oneSignal

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import storage from './src/storages/store';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
 
 const {store, persistor} = storage();
+
+const AppID = '9fed0bad-fea6-4dc9-b4d5-d23857a12b7b';
 class App extends Component {
+  async componentDidMount() {
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+    OneSignal.initialize(AppID);
+    OneSignal.Notifications.requestPermission(true);
+    OneSignal.Notifications.addEventListener('click', event => {
+      console.log('OneSignal: notification clicked:', event);
+    });
+  }
   render() {
     return (
       <Provider store={store}>
